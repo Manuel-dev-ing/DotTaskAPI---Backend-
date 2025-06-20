e994f44c42a516b4bc5fdc78c22f080fdbdbb1c9
Manuel Tamayo Montero <manueltamayo9765@gmail.com>
2025-06-19 22:14:21 -0600

    personaliza la respuesta para el status code 403

---
+++ DotTaskAPI/Authorization/CustomAuthorizationMiddlewareResultHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace DotTaskAPI.Authorization
+{
+    public class CustomAuthorizationMiddlewareResultHandler : IAuthorizationMiddlewareResultHandler
+    {
+        private readonly AuthorizationMiddlewareResultHandler _defaultHandler = new();
+        public async Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
+        {
+            if (authorizeResult.Forbidden)
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                context.Response.ContentType = "application/json";
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    error = "Acceso denegado. No tienes permisos suficientes para acceder a este recurso."
+                });
+
+                await context.Response.WriteAsync(result);
+                return;
+
+            }
+            await _defaultHandler.HandleAsync(next, context, policy, authorizeResult);
+        }
+    }
+}
